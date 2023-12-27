aa9a239a2d38107534b948736cfe3e872b05d2db
Fouad Mesrane <fouadmesrane@gmail.com>
2023-12-27 11:01:36 -0700

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+console.log("fouad");
+console.log(23);
+
+let firstName = "Asmaa";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let PI = 3.14;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
+

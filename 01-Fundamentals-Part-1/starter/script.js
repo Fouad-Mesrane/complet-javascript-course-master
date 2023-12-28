d6bde3a56d7804bc21dd437acb7efe3abafa1840
Fouad-Mesrane <fouadmesrane@gmail.com>
2023-12-27 20:04:11 -0700

    completed variables and operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 
 console.log(40 + 8 + 23 - 10);
@@ -9,11 +10,55 @@ let firstName = "Asmaa";
 
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+//console.log(firstName);
 let PI = 3.14;
 
 let myFirstJob = "programmer";
 let myCurrentJob = "teacher";
 
 console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun);
+console.log(typeof "hi");
+console.log(typeof 23);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year)
+
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1995;
+// birthYear = 1994; error
+
+// const job;   error
+
+
+
+const firstName = 'Fouad';
+const lastName = 'Mesrane';
+console.log(firstName + " " + lastName);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+
+console.log(x, y);
+const avrageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avrageAge);
+
+*/
 

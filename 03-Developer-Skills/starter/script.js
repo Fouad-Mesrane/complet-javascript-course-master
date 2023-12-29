6830bead5756f3f883e05a581aff10c3a811ec83
Fouad Mesrane <fouadmesrane@gmail.com>
2023-12-29 12:14:36 -0700

    updated vs code json

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const degrees1 = [17, 21, 23];
+const degrees2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `...${arr[i]}C in ${i + 1} days`;
+  }
+  return str;
+};
+
+console.log(printForecast(degrees1));
+console.log(printForecast(degrees2));

30b1a57f3f1be2d32899c82e238ddc388bd161e2
Fouad Mesrane <fouadmesrane@gmail.com>
2023-12-31 10:44:19 -0700

    reset button logic implemented

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,22 +10,27 @@ document.querySelector('.guess').value = 13;
 console.log(document.querySelector('.guess').value); 
 
 */
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
-  } else if (guess === secretNumber) {
+  } // when the answer is correct logic
+   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-
+    highscore = score;
+    if(highscore >= score){
+    document.querySelector('.highscore').textContent = highscore};
+// too high guess logic
   }else if(guess > secretNumber){
     if(score > 1) {
         document.querySelector('.message').textContent = 'Too high';
@@ -36,7 +41,7 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.score').textContent = 0;
     }
 
-    
+    // too low guess logic
   } else if(guess < secretNumber){
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low';
@@ -47,4 +52,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+
 });
+
+  // reset button
+document.querySelector('.again').addEventListener('click', function(){
+    secretNumber = Math.trunc(Math.random() * 20 + 1); 
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+  //  document.getElementsByClassName('.guess').value = '0';
+    document.querySelector('body').style.backgroundColor=('#222');
+    score = 20;
+    document.querySelector('.score').textContent = score;
+})

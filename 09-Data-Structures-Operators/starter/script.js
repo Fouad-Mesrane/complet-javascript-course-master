0181072e968e88d008ec28aee8614ade3ff66470
Fouad Mesrane <fouadmesrane@gmail.com>
2024-01-22 12:39:13 -0700

    finished strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -2,7 +2,6 @@
 
 // Data needed for a later exercise
 
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -356,11 +355,10 @@ btn.addEventListener('click', function () {
     const output = `${
       first + second.replace(second[0], second[0].toUpperCase())
     }`;
-    
+
     console.log(`${output.padEnd(20)} ${'✅'.repeat(rows.indexOf(row) + 1)}`);
   }
 });
-
 
 // 🔴 Delayed Departure from FAO to TXL (11h25)
 //              Arrival from BRU to FAO (11h45)
@@ -370,11 +368,13 @@ btn.addEventListener('click', function () {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-
-  for (const flight of flights.split('+')){
-    const [type, from, to, time] = flight.split(';');
-    const output = `${type} ${from} ${to} (${time.re})`;
-    console.log(output);
-    
-  }
-  
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴 ' : ''}${type.replaceAll(
+    '_',
+    ' '
+  ).trim()} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(50);
+  console.log(output);
+}

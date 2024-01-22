159aa3c2ae5d42f99dbc9e9eb1132d42ad9f764d
fouad mesrane <fouadmesrane@gmail.com>
2024-01-22 08:27:54 -0700

    string challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -370,4 +370,11 @@ btn.addEventListener('click', function () {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+
+  for (const flight of flights.split('+')){
+    const [type, from, to, time] = flight.split(';');
+    const output = `${type} ${from} ${to} (${time.re})`;
+    console.log(output);
+    
+  }
   
